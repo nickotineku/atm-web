c700f6d78f1f92e888ef648611dd6dab5f8f3f76
Nickotineku <nick.va@ku.ac.th>
2021-02-27 13:02:25 +0700

    Connect to Database with JPA

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -2,33 +2,34 @@ package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CustomerService {
 
     //responsible for processing and managing customer information
     //customer information
-    private List<Customer> customerList;
 
-    @PostConstruct
-    public void postConstruct(){
-        this.customerList = new ArrayList<>();
+    private CustomerRepository repository;
+
+    public CustomerService(CustomerRepository repository){
+        this.repository = repository;
     }
 
     public void createCustomer(Customer customer){
         //...hash pin for customer...
         String hashpin = hash(customer.getPin());
         customer.setPin(hashpin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomers(){
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     private String hash(String pin){
@@ -38,11 +39,12 @@ public class CustomerService {
     }
 
     public Customer findCustomer(int id){
-        for(Customer customer : customerList){
-            if( customer.getId()== id)
-                return customer;
-        }
-        return null;
+       try {
+           return  repository.findById(id).get();
+       }catch (NoSuchElementException e){
+           return null;
+       }
+
     }
 
     public Customer checkPin(Customer inputCustomer){
